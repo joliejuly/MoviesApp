d1100cc6d5de83a2966467ac27775a7c061fc7ec
Julia Nikitina <joliejuly@gmail.com>
2025-10-04 15:35:54 +0200

    Add navtitle for about page

--- MoviesApp/Presentation/About/AboutPageView.swift
+++ MoviesApp/Presentation/About/AboutPageView.swift
@@ -3,11 +3,15 @@ import WebKit
 
 /// A simple SwiftUI wrapper around `WKWebView` for iOS below 26
 struct AboutPageView: View {
+    
     private let readmeURL = URL(string: "https://github.com/joliejuly/MoviesApp/blob/main/README.md")!
     
     
     var body: some View {
-        WebView(url: readmeURL)
-            .ignoresSafeArea()
+        NavigationView {
+            WebView(url: readmeURL)
+                .navigationTitle("About the app")
+        }
+        
     }
 }
